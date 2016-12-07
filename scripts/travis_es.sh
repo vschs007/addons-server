26ab05fc7ce28e742ea575dbfc856c1c076e9a56
Mathieu Pillard <mpillard@mozilla.com>
2016-12-07 09:21:35 -1000

    Bump elasticsearch in travis to 2.3.3

--- scripts/travis_es.sh
+++ scripts/travis_es.sh
@@ -2,12 +2,12 @@
 
 TARGET="/tmp/elasticsearch"
 
-if [ ! -f "$TARGET/elasticsearch-1.6.4/bin/elasticsearch" ]; then
+if [ ! -f "$TARGET/elasticsearch-2.3.3/bin/elasticsearch" ]; then
     echo "$TARGET not found. Building..."
     mkdir -p $TARGET
     cd $TARGET
-    wget --no-check-certificate https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.6.2.tar.gz
-    tar xvf elasticsearch-1.6.2.tar.gz
+    wget --no-check-certificate https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-2.3.3.tar.gz
+    tar xvf elasticsearch-2.3.3.tar.gz
 else
     echo "$TARGET already exists"
 fi
