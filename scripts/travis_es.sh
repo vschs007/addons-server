222d092b8332b576bb4996716a973d8d4774a8fc
Christopher Grebs <cg@webshox.org>
2016-12-08 18:12:16 -1000

    Install es 5.1.1

--- scripts/travis_es.sh
+++ scripts/travis_es.sh
@@ -2,12 +2,12 @@
 
 TARGET="/tmp/elasticsearch"
 
-if [ ! -f "$TARGET/elasticsearch-2.3.3/bin/elasticsearch" ]; then
+if [ ! -f "$TARGET/elasticsearch-5.1.1/bin/elasticsearch" ]; then
     echo "$TARGET not found. Building..."
     mkdir -p $TARGET
     cd $TARGET
-    wget --no-check-certificate https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-2.3.3.tar.gz
-    tar xvf elasticsearch-2.3.3.tar.gz
+    wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.1.1.tar.gz
+    tar xvf elasticsearch-5.1.1.tar.gz
 else
     echo "$TARGET already exists"
 fi
