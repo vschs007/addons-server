180c17ab3420ace91d0b626da83cee25b80e337e
Christopher Grebs <cg@webshox.org>
2017-04-19 22:49:14 +0700

    Add comment, questioning custom index settings

--- src/olympia/search/indexers.py
+++ src/olympia/search/indexers.py
@@ -19,13 +19,19 @@ indexers = (AddonIndexer, AppCompatIndexer, CollectionIndexer,
             UserProfileIndexer)
 
 # Search-related index settings.
+# TODO: Is this still needed? Do we care?
+# re https://github.com/mozilla/addons-server/issues/2661
+# funny enough, the elasticsearch mention that `dictionary_decompounder` is to
+# "decompose compound words found in many German languages" -
+# and all the words in the list are English… (cgrebs 042017)
 INDEX_SETTINGS = {
     'analysis': {
         'analyzer': {
             'standardPlusWordDelimiter': {
                 'tokenizer': 'standard',
-                'filter': ['standard', 'wordDelim', 'lowercase', 'stop',
-                           'dict']
+                'filter': [
+                    'standard', 'wordDelim', 'lowercase', 'stop', 'dict'
+                ]
             }
         },
         'filter': {
